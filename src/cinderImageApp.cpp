02bcbe6856a7ca9e26831b8328f28b40f8242ebc
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2014-05-18 14:50:02 -0700

    added a resize function

--- src/cinderImageApp.cpp
+++ src/cinderImageApp.cpp
@@ -2,9 +2,11 @@
 #include "cinder/ImageIo.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
+#include "cinder/ip/Resize.h"
 
 using namespace ci;
 using namespace ci::app;
+using namespace ci::ip;
 using namespace std;
 
 class cinderImageApp : public AppNative {
@@ -15,6 +17,7 @@ class cinderImageApp : public AppNative {
     
     Surface pixelKidSurface; //actually Surface8u(nsigned int) there's also a Surface32f(loat)
     //perform C++ operations on a surface. Texture is for GPU
+    Surface pixelKidResize;
     gl::Texture pixelKidTexture;
 };
 
@@ -30,25 +33,32 @@ void cinderImageApp::setup()
         cout << "no Alpha Channel!\n";
         //can't force format. don't fucking ask me why.
     }
+    if (pixelKidSurface.isPremultiplied())
+    {
+        cout << "Premultiplied by its alpha channel!!\n";
+    }
     
+    pixelKidResize = resizeCopy(pixelKidSurface, pixelKidSurface.getBounds(), Vec2i(app::getWindowWidth(), app::getWindowHeight()));
+    int pkrw = pixelKidSurface.getWidth();
+    int pkrh = pixelKidSurface.getHeight();
+    cout << "width:" << pkrw << ", height:" << pkrh << "\n";
     
-    Area pkarea(0, 0, pkw, pkh);
-    Surface::Iter iter = pixelKidSurface.getIter( pkarea );
+    Area pkrarea(0, 0, pkrw, pkrh);
+    Surface::Iter iter = pixelKidResize.getIter( pkrarea );
     while (iter.line())
     {
         while (iter.pixel())
         {
-            iter.r() = iter.r(); //iter.r returns a reference so we can use it.
-            iter.g() = iter.g();
-            iter.b() = iter.b();
+            iter.r() = 255 - iter.r(); //iter.r returns a reference so we can use it.
+            iter.g() = 255 - iter.g();
+            iter.b() = 255 - iter.b();
         }
     }
 
     
 
-    pixelKidTexture = gl::Texture(pixelKidSurface);
+    pixelKidTexture = gl::Texture(pixelKidResize);
     pixelKidTexture.setWrap(GL_REPEAT, GL_REPEAT);
-    //pixelKidTexture.setWrap(GL_REPEAT, GL_REPEAT); //x, y
     //pixelKidTexture.enableMipmapping
     
     
--- xcode/build/cinderImage.build/Debug/cinderImage.build/Objects-normal/i386/cinderImageApp.d
+++ xcode/build/cinderImage.build/Debug/cinderImage.build/Objects-normal/i386/cinderImageApp.d
@@ -1,4 +1,6 @@
 dependencies: \
   /Users/wolfgag/cinderProjects/cinderImage/src/cinderImageApp.cpp \
   ../../../cinder_0.8.5_mac/include/cinder/app/AppNative.h \
-  ../../../cinder_0.8.5_mac/include/cinder/gl/Texture.h
+  ../../../cinder_0.8.5_mac/include/cinder/gl/Texture.h \
+  ../../../cinder_0.8.5_mac/include/cinder/ip/Resize.h \
+  ../../../cinder_0.8.5_mac/include/cinder/Filter.h
